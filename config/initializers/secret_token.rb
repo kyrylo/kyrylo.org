27e24707677adcf85e936082d1ecfcf6783d924c
Kyrylo Silin <kyrylosilin@gmail.com>
2014-03-12 15:16:42 +0200

    Onwards to my dream!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Kyrylo::Application.config.secret_key_base = '804d3394d770c4d7733225a23a1ce7486d74578e4a9109aaf4c83a93ad9cc1ed566c435dbd6c435eb0e6ddc36536e86a5aeb1dc836db1e7dadd3824e052ed0fc'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Kyrylo::Application.config.session_store :cookie_store, key: '_kyrylo_session'
