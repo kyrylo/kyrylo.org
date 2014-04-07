09cdc0b112bc650bf4bd03f517b614e2f507fec8
Kyrylo Silin <kyrylosilin@gmail.com>
2014-04-07 03:18:27 +0300

    Convert the syntax to RSpec
    Delete also the unwanted file

--- spec/views/layouts/application.html.slim_spec.rb
+++ spec/views/layouts/application.html.slim_spec.rb
@@ -2,16 +2,16 @@ require 'spec_helper'
 
 describe "layouts/application" do
   before do
-    view.stub('current_page?'.to_sym) { |opts| url_for(opts) == root_path }
+    allow(view).to receive(:current_page?) { |opts| url_for(opts) == root_path }
     render
   end
 
   it "has a linkless logotype" do
-    assert_select 'header>img'
-    assert_select 'header>a>img', count: 0
+    expect(rendered).to have_selector('header>img')
+    expect(rendered).to have_selector('header>a>img', count: 0)
   end
 
   it "has a menu entry with the link" do
-    assert_select 'nav>ul>li>a', text: 'Projects'
+    expect(rendered).to have_selector('nav>ul>li>a', text: 'Projects')
   end
 end
--- spec/views/pages/home/index.html.slim_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'spec_helper'
-
-describe "layouts/application" do
-  it "creates a menu entry with the link" do
-    render
-    assert_select 'nav>ul>li>a', text: 'Projects'
-  end
-end
