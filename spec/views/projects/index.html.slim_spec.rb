ab552c13c8a8a1a4b5c4dee04d132fa562939bcf
Kyrylo Silin <kyrylosilin@gmail.com>
2014-04-14 02:36:34 +0300

    Test project/index views

--- spec/views/projects/index.html.slim_spec.rb
+++ spec/views/projects/index.html.slim_spec.rb
@@ -1,25 +1,38 @@
 require 'spec_helper'
 
 describe "projects/index" do
-  # before(:each) do
-  #   assign(:projects, [
-  #     stub_model(Project,
-  #       :title => "MyText",
-  #       :headline => "MyText",
-  #       :description => "MyText"
-  #     ),
-  #     stub_model(Project,
-  #       :title => "MyText",
-  #       :headline => "MyText",
-  #       :description => "MyText"
-  #     )
-  #   ])
-  # end
+  let(:projects) {
+    4.times.map { FactoryGirl.create(:project) } +
+    3.times.map { FactoryGirl.create(:project_with_thumbnail) }
+  }
 
-  # it "renders a list of projects" do
-  #   render
-  #   assert_select "tr>td", :text => "MyText".to_s, :count => 2
-  #   assert_select "tr>td", :text => "MyText".to_s, :count => 2
-  #   assert_select "tr>td", :text => "MyText".to_s, :count => 2
-  # end
+  before do
+    assign(:projects, projects)
+    assign(:projects_count, 7)
+    render
+  end
+
+  it "has a title" do
+    expect(rendered).to have_selector('h1', text: 'Projects')
+  end
+
+  it "has the projects counter" do
+    expect(rendered).to have_selector('.projects-meta .quantity div', text: '7')
+  end
+
+  it "renders a list of projects" do
+    expect(rendered).to have_selector('.thumbnail', count: 7)
+  end
+
+  it "represents thumbnails in rows with 4 elements" do
+    expect(rendered).to have_selector('.row', count: 2)
+  end
+
+  it "builds the project's thumbnail as an image" do
+    expect(rendered).to have_selector('.thumbimg img', count: 3)
+  end
+
+  it "builds the project's thumbnail as text" do
+    expect(rendered).to have_selector('.thumbimg .pseudoimg', count: 4)
+  end
 end
