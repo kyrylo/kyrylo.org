915b5b3589e33acd7ee0423c067922e3a350d8f1
Kyrylo Silin <kyrylosilin@gmail.com>
2014-04-24 13:49:15 +0300

    third_party_spec.rb: refactor tests

--- spec/models/third_party_spec.rb
+++ spec/models/third_party_spec.rb
@@ -1,11 +1,8 @@
 require 'spec_helper'
 
 describe ThirdParty do
-  it "is invalid without a name" do
-    expect(FactoryGirl.build(:third_party, name: nil)).to be_invalid
-  end
-
-  it "is valid without a link" do
-    expect(FactoryGirl.build(:third_party, link: nil)).to be_valid
+  describe "validations" do
+    it { should validate_presence_of :name }
+    it { should_not validate_presence_of :link }
   end
 end
