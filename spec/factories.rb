01e4d91e00187c3c601d60eb5e7c9d732fe11f4f
Kyrylo Silin <silin@kyrylo.org>
2017-08-20 13:40:28 +0300

    rubocop: fix offences of the Layout/IndentationConsistency cop

--- spec/factories.rb
+++ spec/factories.rb
@@ -4,35 +4,35 @@ FactoryGirl.define do factory :devlog_entry do
     markdown 'MyText'
                        end
 
-                       sequence :project_title, aliases: [:name] do
-                         Faker::Commerce.product_name
-                       end
+                      sequence :project_title, aliases: [:name] do
+                        Faker::Commerce.product_name
+                      end
 
-                       sequence :headline do
-                         Faker::Company.catch_phrase
-                       end
+                      sequence :headline do
+                        Faker::Company.catch_phrase
+                      end
 
-                       sequence :paragraph do
-                         Faker::Lorem.paragraph
-                       end
+                      sequence :paragraph do
+                        Faker::Lorem.paragraph
+                      end
 
-                       sequence :word do
-                         Faker::Lorem.word
-                       end
+                      sequence :word do
+                        Faker::Lorem.word
+                      end
 
-                       sequence :short_text do
-                         Faker::Lorem.paragraph(3)
-                       end
+                      sequence :short_text do
+                        Faker::Lorem.paragraph(3)
+                      end
 
-                       sequence :link do
-                         Faker::Internet.url
-                       end
+                      sequence :link do
+                        Faker::Internet.url
+                      end
 
-                       sequence :website do
-                         Faker::Internet.domain_name
-                       end
+                      sequence :website do
+                        Faker::Internet.domain_name
+                      end
 
-                       sequence :full_name do
-                         Faker::Name.name
-                       end
+                      sequence :full_name do
+                        Faker::Name.name
+                      end
 end
