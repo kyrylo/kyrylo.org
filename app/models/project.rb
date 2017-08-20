8afb11f2cee3c6af961fc2b4f94bb1f5977e3d3b
Kyrylo Silin <silin@kyrylo.org>
2017-08-20 14:04:04 +0300

    rubocop: fix offences of the Metrics/LineLength cop

--- app/models/project.rb
+++ app/models/project.rb
@@ -8,32 +8,39 @@ class Project < ActiveRecord::Base
     'Entooru' => {
       link: 'https://github.com/kyrylo/entooru',
       date: 'April 16, 2012',
-      desc: 'A collection of my translations of various Ruby articles into the Russian language.'
+      desc: 'A collection of my translations of various Ruby articles into ' \
+            'the Russian language.'
     },
     'Pry Theme' => {
       link: 'https://github.com/kyrylo/pry-theme',
       date: 'June 26, 2012',
-      desc: 'A plugin for the Pry REPL for the Ruby programming language, which allows to customise Pry colors via prytheme.rb files.'
+      desc: 'A plugin for the Pry REPL for the Ruby programming language, ' \
+            'which allows to customise Pry colors via prytheme.rb files.'
     },
     'Fast Method Source' => {
       link: 'https://github.com/kyrylo/fast_method_source',
       date: 'June 18, 2015',
-      desc: 'A Ruby C extension for querying methods, procs and lambdas for their source code and comments. Extremely fast.'
+      desc: 'A Ruby C extension for querying methods, procs and lambdas for ' \
+            'their source code and comments. Extremely fast.'
     },
     'System Browser' => {
       link: 'https://github.com/kyrylo/system_browser_client',
       date: 'July 21, 2015',
-      desc: 'A desktop app for browsing Ruby code. Just click to get details about a module or class and explore its namespace or see the source of a method.'
+      desc: 'A desktop app for browsing Ruby code. Just click to get details ' \
+            'about a module or class and explore its namespace or see the ' \
+            'source of a method.'
     },
     'System Navigation' => {
       link: 'https://github.com/kyrylo/system_navigation',
       date: 'June 11, 2015',
-      desc: 'A library for the Ruby programming language that provides navigation and introspection capabilities for Ruby programs.'
+      desc: 'A library for the Ruby programming language that provides ' \
+            'navigation and introspection capabilities for Ruby programs.'
     },
     'RMarshal' => {
       link: 'https://github.com/kyrylo/rmarshal',
       date: 'December 18, 2014',
-      desc: 'An Erlang library for deserialising Ruby objects dumped by Marshal.dump into Erlang terms.'
+      desc: 'An Erlang library for deserialising Ruby objects dumped by ' \
+            'Marshal.dump into Erlang terms.'
     }
   }
 
