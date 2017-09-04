be93b45bedf54059a5736f33c5667f3a6a0e483d
Kyrylo Silin <silin@kyrylo.org>
2017-09-05 01:55:12 +0300

    db/migrate/20170903233141_drop_projects: fix Rubocop offence

--- db/migrate/20170903233141_drop_projects.rb
+++ db/migrate/20170903233141_drop_projects.rb
@@ -4,14 +4,14 @@ class DropProjects < ActiveRecord::Migration
 
     drop_table 'projects', force: :cascade do |t|
       t.date     'release_date'
-      t.boolean  'released',     default: true
+      t.boolean  'released', default: true
       t.text     'title'
       t.text     'html'
       t.text     'markdown'
       t.text     'slug'
       t.text     'description'
-      t.datetime 'created_at',                  null: false
-      t.datetime 'updated_at',                  null: false
+      t.datetime 'created_at', null: false
+      t.datetime 'updated_at', null: false
     end
   end
 end
