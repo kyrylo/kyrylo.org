7bac6b4bb930595e3e485e5e0e00730b62cd432d
Kyrylo Silin <silin@kyrylo.org>
2017-09-20 00:08:35 +0300

    Delete unused functions

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -6,24 +6,6 @@ function ready(fn) {
   }
 }
 
-function addClass(el, className) {
-  if (el.classList) {
-    el.classList.add(className);
-  } else {
-    el.className += ' ' + className;
-  }
-}
-
-function removeClass(el, className) {
-  if (el.classList) {
-    el.classList.remove(className);
-  } else {
-    el.className = el.className.replace(
-      new RegExp('(^|\\b)' + className.split(' ').join('|') + '(\\b|$)', 'gi'), ' '
-    );
-  }
-}
-
 function toggleClass(el, className) {
   if (el.classList) {
     el.classList.toggle(className);
