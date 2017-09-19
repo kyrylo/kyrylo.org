f6263b3aacf1c7dad2e8458acd358c71ce706318
Kyrylo Silin <silin@kyrylo.org>
2017-09-19 22:55:45 +0300

    Add missing.png

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,6 +1,8 @@
 class Trip < ActiveRecord::Base
   extend FriendlyId
   friendly_id :title, use: %i[slugged history finders]
-  has_attached_file :thumb, styles: { thumb: '200x150#' }
+  has_attached_file :thumb,
+                    styles: { thumb: '200x150#' },
+                    default_url: '/images/missing.png'
   validates_attachment_content_type :thumb, content_type: /\Aimage\/.*\z/
 end
