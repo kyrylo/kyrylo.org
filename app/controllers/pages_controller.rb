5f9737da578d9e993ac3b67d7cbcaccdf80c3683
Kyrylo Silin <silin@kyrylo.org>
2017-10-02 01:49:31 +0300

    controllers/pages: cleanup

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,44 +1,10 @@
 class PagesController < ApplicationController
   def home
     @posts = Post.all.order(created_at: :desc)
-    @trips = Trip.all.sort_by(&:when_end).reverse
+    @trips = Trip.all.order(when_end: :desc)
   end
 
   def cv
     @title = 'Curriculum Vitae'
-
-    cv_path = Rails.root + 'public/cv.pdf'
-    raw_pdf_size = File.size(cv_path).to_f / 2**20
-    @pdf_size = '(%.2f MiB)' % raw_pdf_size
-
-    mp3_path = Rails.root + 'public/kyrylo.mp3'
-    raw_mp3_size = File.size(mp3_path).to_f / 2**20
-    @mp3_size = '(%.3f MiB)' % raw_mp3_size
-  end
-
-  private
-
-  def timeline_records
-    Post.all
-  end
-
-  def sorted_timeline_records
-    timeline_records.sort_by do |record|
-      if record.is_a?(Trip)
-        record.when_start
-      else
-        record.created_at
-      end
-    end
-  end
-
-  def grouped_timeline_records
-    sorted_timeline_records.group_by do |record|
-      if record.is_a?(Trip)
-        record.when_start.year
-      else
-        record.created_at.year
-      end
-    end
   end
 end
