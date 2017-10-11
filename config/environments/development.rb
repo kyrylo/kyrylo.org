2ef5e0144ede67003e7ad34a3c1d9b3bbbcadac7
Kyrylo Silin <silin@kyrylo.org>
2017-10-12 02:58:49 +0300

    Use SCSS, not SASS

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,7 +27,4 @@ Kyrylo::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
-
-  # Use SASS, not SCSS. https://coderwall.com/p/uklpaa
-  config.sass.preferred_syntax = :sass
 end
