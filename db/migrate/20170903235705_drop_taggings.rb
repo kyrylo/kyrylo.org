dbe48507af479c361060456fd29865fc126722c5
Kyrylo Silin <silin@kyrylo.org>
2017-11-17 00:49:21 +0200

    Fix migrations 3

--- db/migrate/20170903235705_drop_taggings.rb
+++ db/migrate/20170903235705_drop_taggings.rb
@@ -1,11 +1,5 @@
 class DropTaggings < ActiveRecord::Migration[4.2]
   def change
-    remove_index(
-      'taggings',
-      column: %i[tag_id taggable_id taggable_type context tagger_id tagger_type],
-      name: 'index_taggings_on_taggable_id_and_taggable_type_and_context'
-    )
-
     drop_table 'taggings', force: :cascade do |t|
       t.integer  'tag_id'
       t.integer  'taggable_id'
