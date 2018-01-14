3604b9370b7936747aca97eaaacef495b37adb5c
Kyrylo Silin <silin@kyrylo.org>
2018-01-14 23:05:15 +0800

    models/trip: use file storage for paperclip
    The imgur storage would be my preferrable choice, however the adapter is broken.

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -2,7 +2,6 @@ class Trip < ActiveRecord::Base
   extend FriendlyId
   friendly_id :title, use: %i[slugged history finders]
   has_attached_file :thumb,
-                    storage: :imgur,
                     styles: { thumb: '200x150#' },
                     default_url: '/images/missing.png'
   validates_attachment_content_type :thumb, content_type: /\Aimage\/.*\z/
