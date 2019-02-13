c167570482177a726f812c49e33b8c56fd36f5ca
Kyrylo Silin <silin@kyrylo.org>
2019-02-14 00:50:52 +0200

    Make it possible to run this app with Unicorn

--- app.rb
+++ app.rb
@@ -1,5 +1,5 @@
-require 'bundler'
-Bundler.require
+require 'redcarpet'
+require 'sinatra/base'
 
 Tilt.register(Redcarpet, 'md')
 
@@ -7,31 +7,31 @@ class SmartHTML < Redcarpet::Render::HTML
   include Redcarpet::Render::SmartyPants
 end
 
-set(
-  :markdown,
-  renderer: SmartHTML,
-  fenced_code_blocks: true,
-  superscript: true,
-  footnotes: true,
-  no_intra_emphasis: true,
-  lax_html_blocks: true,
-  tables: true,
-  autolink: true,
-  strikethrough: true
-)
+class App < Sinatra::Base
+  set(
+    :markdown,
+    renderer: SmartHTML,
+    fenced_code_blocks: true,
+    superscript: true,
+    footnotes: true,
+    no_intra_emphasis: true,
+    lax_html_blocks: true,
+    tables: true,
+    autolink: true,
+    strikethrough: true
+  )
 
-get '/' do
-  erb :index
+  get '/' do
+    erb :index
+  end
+
+  get '/:year/:month/:day/:title' do
+    file = "#{File.dirname(__FILE__)}/posts/#{params.values.join('/')}.md"
+    pass unless File.exist?(file)
+
+    post = File.read(file)
+    @title = "#{post.lines.first.chomp} &mdash; Kyrylo Silin"
+
+    erb :post, locals: { post: post }
+  end
 end
-
-get '/:year/:month/:day/:title' do
-  file = "#{File.dirname(__FILE__)}/posts/#{params.values.join('/')}.md"
-  pass unless File.exist?(file)
-
-  post = File.read(file)
-  @title = "#{post.lines.first.chomp} &mdash; Kyrylo Silin"
-
-  erb :post, locals: { post: post }
-end
-
-run Sinatra::Application.run!
